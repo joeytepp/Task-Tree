fd57abe82a902d0fc8e71b6b1f3733990f124363
Joey <jmichaelt98@gmail.com>
2019-04-28 20:09:53 -0400

    Add resolvable fields for projects and tasks

--- app/graphql/types/project_type.rb
+++ app/graphql/types/project_type.rb
@@ -8,5 +8,11 @@ module Types
     field :color, Enums::ProjectColors, null: false, description: "The color of the project."
     field :created_at, String, null: false, description: "The time at which the project was created."
     field :updated_at, String, null: false, description: "The time at which the project was last updated."
+
+    field :tasks, [TaskType], null: false, description: "The tasks in the project."
+
+    def tasks
+      object.tasks
+    end
   end
 end
--- app/graphql/types/task_type.rb
+++ app/graphql/types/task_type.rb
@@ -4,8 +4,29 @@ module Types
   class TaskType < BaseObject
     field :id, type: String, null: false, description: "The identifier of the task."
     field :name, type: String, null: false, description: "The name of the task."
+    field :project, type: ProjectType, null: false, description: "The project that the task is in."
+    field :children, type: [TaskType], null: false, description: "The children of the task."
+    field :parent, type: TaskType, null: true, description: "The parent of the task."
+    field :root, type: TaskType, null: true, description: "The root of the task."
     field :completed, type: Boolean, null: false, description: "The completion status of the task."
     field :created_at, type: String, null: false, description: "The time at which the task was created."
     field :updated_at, type: String, null: false, description: "The time at which the task was last updated."
+
+
+    def project
+      object.project
+    end
+
+    def children
+      object.children
+    end
+
+    def parent
+      object.parent
+    end
+
+    def root
+      object.root
+    end
   end
 end
