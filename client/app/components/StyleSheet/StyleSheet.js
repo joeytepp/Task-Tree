cd3321e00dc07df6bb6ed94400de6842de77d112
Joey <jmichaelt98@gmail.com>
2019-05-03 23:41:05 -0400

    Fix server side css loading bug

--- client/app/components/StyleSheet/StyleSheet.js
+++ client/app/components/StyleSheet/StyleSheet.js
@@ -3,30 +3,27 @@ import { renderToString } from "react-dom/server";
 import { ServerStyleSheet, StyleSheetManager } from "styled-components";
 
 export default Component => props => {
+  const sheet = new ServerStyleSheet();
+  let componentHtml;
+
   try {
-    const sheet = new ServerStyleSheet();
-    const componentHtml = renderToString(
+    componentHtml = renderToString(
       <StyleSheetManager sheet={sheet.instance}>
         <Component {...props} />
       </StyleSheetManager>
     );
-
-    const componentCss = sheet.getStyleTags();
-
-    sheet.seal();
-
-    return {
-      renderedHtml: {
-        componentHtml,
-        componentCss
-      }
-    };
   } catch (err) {
-    return {
-      renderedHtml: {
-        componentHtml: "",
-        componentCss: ""
-      }
-    };
+    componentHtml = "<div></div>";
   }
+
+  const componentCss = sheet.getStyleTags();
+
+  sheet.seal();
+
+  return {
+    renderedHtml: {
+      componentHtml,
+      componentCss
+    }
+  };
 };
