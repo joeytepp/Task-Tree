3a65e7539f29258e60f887294ac5bdd2029e97df
Joey <jmichaelt98@gmail.com>
2020-03-21 14:41:27 -0400

    Add seeds to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = User.create([
+  {
+    first_name: "Joe",
+    last_name: "Smith",
+    email: "joe@yopmail.com",
+    password: "password",
+    password_confirmation: "password",
+    terms_of_service: true
+  },
+  {
+    first_name: "Bob",
+    last_name: "Smith",
+    email: "bob@yopmail.com",
+    password: "password",
+    password_confirmation: "password",
+    terms_of_service: true
+  }
+])
+
+projects = Project.create([{
+    name: "Joe's Project",
+  },
+  {
+    name: "Bob's Project"
+  },
+  {
+    name: "Joe and Bob's Project",
+    color: 2
+}])
+
+project_users = []
+
+users.each_with_index do |user, i|
+  project_users = project_users | [{user_id: user.id, project_id: projects[i].id}, {user_id: user.id, project_id: projects[-1].id}]
+end
+
+ProjectUser.create(project_users)
